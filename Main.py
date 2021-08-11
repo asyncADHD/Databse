fa7e2a4ab2b9d863e1efcf8ae4f3586c2011cba8
callum thomas <callumthomas086@gmail.com>
2021-08-11 13:42:47 +0100

    v0.02

--- Main.py
+++ Main.py
@@ -22,22 +22,47 @@ c = conn.cursor()
 
 # Create table
 '''
+
 c.execute("""CREATE TABLE adresses(
     first_name text,
     Last_name text,
     Adress text,
+    City,
     Postcode text
     )""")
-
 '''
+
 
 
 # Create submit function 
 
 def submit():
+    conn = sqlite3.connect('Adress_book.db')
+    
+    # Create cursor
+    c = conn.cursor()
+    
+    # Insert Into table 
+    c.execute("INSERT INTO adresses VALUES(:f_name, :last_name, :adress, :City, :postcode)",
+        {
+            'f_name': f_name.get(),
+            'last_name': last_name.get(),
+            'adress': adress.get(),
+            'City': City.get(),
+            'postcode': postcode.get()
+        })
+
+    # Commit Changes 
+    conn.commit()
+
+    # Close changes 
+    conn.close()
+    
+    # Clear the boxes
     f_name.delete(0, END)
     last_name.delete(0, END)
     City.delete(0, END)
+    adress.delete(0, END)
     postcode.delete(0, END)
 
 f_name = Entry(root, width= 30)
@@ -58,19 +83,19 @@ postcode.grid(row=4,column=1, padx=20)
 
 
 # Create textbox labels
-f_name_lable = Label(root, text="first name")
+f_name_lable = Label(root, text="First name")
 f_name_lable.grid(row=0,column=0)
 
-last_name_lable = Label(root, text="first name")
+last_name_lable = Label(root, text="Last name")
 last_name_lable.grid(row=1,column=0)
 
-f_name_lable = Label(root, text="first name")
-f_name_lable.grid(row=2,column=0)
+adress_lable = Label(root, text="Adress")
+adress_lable.grid(row=2,column=0)
 
-city_lable = Label(root, text="first name")
+city_lable = Label(root, text="City")
 city_lable.grid(row=3,column=0)
 
-postcode_lable = Label(root, text="first name")
+postcode_lable = Label(root, text="Postcode")
 postcode_lable.grid(row=4,column=0)
 
 
